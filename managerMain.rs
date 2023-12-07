73f08a3a0dd9bcb635f37db4403d93f34c5eca88
Paul Gaddy <35400575+notpaulgaddy@users.noreply.github.com>
2023-12-06 20:25:02 -0500

    Can now save data to txt file

--- managerMain.rs
+++ managerMain.rs
@@ -1,28 +1,29 @@
 use std::collections::HashMap;
 use std::io;
+use std::fs::File;
+use std::io::Write;
 
 fn main() {
     let mut start = String::new();
     let mut my_map = HashMap::new();
-
     println!("Would you like to use the password manager? (yes or no)");
     io::stdin()
         .read_line(&mut start)
         .expect("Failed to read line");
-
-    start = start.trim().to_lowercase();
+    
+    start = start.trim().to_lowercase().to_string();
 
     if start == "yes" {
         let mut input = String::new();
 
         while input.trim() != "end" {
-            println!("Enter a command (add, end):");
+            println!("Enter a command (add, view, update, delete, end):");
             input.clear();
             io::stdin()
                 .read_line(&mut input)
                 .expect("Failed to read line");
 
-            input = input.trim().to_lowercase();
+            let input = input.trim().to_lowercase();
 
             match input.as_str() {
                 "add" => {
@@ -44,7 +45,10 @@ fn main() {
                         .read_line(&mut password)
                         .expect("Failed to read line");
 
-                    my_map.insert(app_name.trim().to_string(), (username.trim().to_string(), password.trim().to_string()));
+                    my_map.insert(
+                        app_name.trim().to_string(),
+                        (username.trim().to_string(), password.trim().to_string()),
+                    );
                 }
                 "view" => {
                     for (app_name, (username, password)) in &my_map {
@@ -58,52 +62,112 @@ fn main() {
                     println!("Goodbye!");
                     break;
                 }
-                _ => {
-                    println!("Unknown command: {}", input);
-                }
                 "update" => {
-                    let mut appName = String::new();
-                    println("What is the name of the application you'd like to update?")
-                    appName = appName.trim().to_string();
+                    println!("What is the name of the application you'd like to update?");
+                    let mut app_name = String::new();
                     io::stdin()
                         .read_line(&mut app_name)
                         .expect("Failed to read line");
-                    if my_map.contains_key(&appName){
-                        println("Would you like to update the username or password for {}?", appName);
+                    let app_name = app_name.trim().to_string();
+
+                    if my_map.contains_key(&app_name) {
+                        println!(
+                            "Would you like to update the username or password for {}?",
+                            app_name
+                        );
                         let mut choice = String::new();
-                        choice = choice.trim().to_string();
                         io::stdin()
-                            .read_line(&mut new_username)
+                            .read_line(&mut choice)
                             .expect("Failed to read line");
-                        match choice {
+                        let choice = choice.trim().to_lowercase();
+
+                        match choice.as_str() {
                             "username" => {
-                                let mut newUsername = String:new();
+                                println!("Enter the new username:");
+                                let mut new_username = String::new();
                                 io::stdin()
-                                    .read_line(&mut newUsername)
+                                    .read_line(&mut new_username)
                                     .expect("Failed to read line");
-                                if let Some(entry) = my_map.get_mut(&appName){
-                                    entry.0 = newUsername.trim().to_string();
-                                    println("Username updated successfully")
-                                }else{
-                                    println("Failed to update username for {}", appName)
-                                }
-                            },
-                            "password" => {
-                                let mut newPassword = String:new();
-                                io::stdin()
-                                    .read_line(&mut newPassword)
-                                    .expect("Failed to read line");
-                                if let Some(entry) = my_map.get_mut(&appName){
-                                    entry.0 = newPassword.trim().to_string();
-                                    println("Password updated successfully")
-                                }else{
-                                    println("Failed to update password for {}", appName)
+
+                                if let Some(entry) = my_map.get_mut(&app_name) {
+                                    entry.0 = new_username.trim().to_string();
+                                    println!("Username updated successfully");
+                                } else {
+                                    println!("Failed to update username for {}", app_name);
                                 }
                             }
+                            "password" => {
+                                println!("Enter the new password:");
+                                let mut new_password = String::new();
+                                io::stdin()
+                                    .read_line(&mut new_password)
+                                    .expect("Failed to read line");
+
+                                if let Some(entry) = my_map.get_mut(&app_name) {
+                                    entry.1 = new_password.trim().to_string();
+                                    println!("Password updated successfully");
+                                } else {
+                                    println!("Failed to update password for {}", app_name);
+                                }
+                            }
+                            _ => {
+                                println!("Unknown choice: {}", choice);
+                            }
                         }
-                    } else{
-                        println("Application {} not found in the password manager.", appName)
+                    } else {
+                        println!("Application {} not found in the password manager.", app_name);
                     }
+                }
+                "delete" => {
+                    println!("What is the name of the application you'd like to delete?");
+                    let mut app_name = String::new();
+                    io::stdin()
+                        .read_line(&mut app_name)
+                        .expect("Failed to read line");
+                    let app_name = app_name.trim().to_lowercase();
+
+                    if my_map.contains_key(&app_name) {
+                        println!("Are you sure you want to delete your saved info for this application? (yes or no)");
+                        let mut choice = String::new();
+                        io::stdin()
+                            .read_line(&mut choice)
+                            .expect("Failed to read line");
+                        let choice = choice.trim().to_lowercase();
+
+                        match choice.as_str() {
+                            "no" => {
+                                // Do nothing, exit the "delete" case.
+                            }
+                            "yes" => {
+                                if my_map.contains_key(&app_name) {
+                                    my_map.remove(&app_name);
+                                    println!("Application {} deleted successfully", app_name);
+                                }
+                            }
+                            _ => {
+                                println!("Invalid choice: {}", choice);
+                            }
+                        }
+                    } else {
+                        println!("Application {} not found in the password manager.", app_name);
+                    }
+                }
+                "save" => {
+                    let mut data_file = File::create("accounts.txt").expect("creation failed");
+                    //format will be Company Name: ["Email": email@gmail.com, "Password": password123]
+                    for (company_name, (email, password)) in &my_map {
+                        let mut output = String::new();
+                        output.push_str(company_name);
+                        output.push(' '); // Add a space
+                        output.push_str(email);
+                        output.push(' '); // Add a space
+                        output.push_str(password);
+                        output.push_str("\n");
+                        data_file.write(output.as_bytes()).expect("write failed");
+                    }
+                }
+                _ => {
+                    println!("Unknown command: {}", input);
                 }
             }
         }
--- readme.md
+++ readme.md
@@ -7,10 +7,10 @@ Value is array of username/email and password
 Save to specific file in folder
 
 Keywords
-Update
-Delete 
+Update ✅
+Delete ✅
 Add ✅
 View ✅
 End ✅
-Save
+Save ✅
 Import
